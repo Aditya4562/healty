0d1f907eb2d7053c5d0f306854ca60fd7f68ee33
Aditya4562 <165054694+Aditya4562@users.noreply.github.com>
2025-12-18 01:43:10 -0800

    Add files via upload

---
+++ Custom.js/custom.js
@@ -0,0 +1,177 @@
+const slider = document.querySelector('.slider');
+
+function activate(e) {
+  const items = document.querySelectorAll('.item');
+  e.target.matches('.next') && slider.append(items[0])
+  e.target.matches('.prev') && slider.prepend(items[items.length-1]);
+}
+
+document.addEventListener('click',activate,false);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// ---------Responsive-navbar-active-animation-----------
+function test(){
+	var tabsNewAnim = $('#navbarSupportedContent');
+	var selectorNewAnim = $('#navbarSupportedContent').find('li').length;
+	var activeItemNewAnim = tabsNewAnim.find('.active');
+	var activeWidthNewAnimHeight = activeItemNewAnim.innerHeight();
+	var activeWidthNewAnimWidth = activeItemNewAnim.innerWidth();
+	var itemPosNewAnimTop = activeItemNewAnim.position();
+	var itemPosNewAnimLeft = activeItemNewAnim.position();
+	$(".hori-selector").css({
+		"top":itemPosNewAnimTop.top + "px", 
+		"left":itemPosNewAnimLeft.left + "px",
+		"height": activeWidthNewAnimHeight + "px",
+		"width": activeWidthNewAnimWidth + "px"
+	});
+	$("#navbarSupportedContent").on("click","li",function(e){
+		$('#navbarSupportedContent ul li').removeClass("active");
+		$(this).addClass('active');
+		var activeWidthNewAnimHeight = $(this).innerHeight();
+		var activeWidthNewAnimWidth = $(this).innerWidth();
+		var itemPosNewAnimTop = $(this).position();
+		var itemPosNewAnimLeft = $(this).position();
+		$(".hori-selector").css({
+			"top":itemPosNewAnimTop.top + "px", 
+			"left":itemPosNewAnimLeft.left + "px",
+			"height": activeWidthNewAnimHeight + "px",
+			"width": activeWidthNewAnimWidth + "px"
+		});
+	});
+}
+$(document).ready(function(){
+	setTimeout(function(){ test(); });
+});
+$(window).on('resize', function(){
+	setTimeout(function(){ test(); }, 500);
+});
+$(".navbar-toggler").click(function(){
+	$(".navbar-collapse").slideToggle(300);
+	setTimeout(function(){ test(); });
+});
+
+
+
+// --------------add active class-on another-page move----------
+jQuery(document).ready(function($){
+	// Get current path and find target link
+	var path = window.location.pathname.split("/").pop();
+
+	// Account for home page with empty path
+	if ( path == '' ) {
+		path = 'index.html';
+	}
+
+	var target = $('#navbarSupportedContent ul li a[href="'+path+'"]');
+	// Add active class to target link
+	target.parent().addClass('active');
+});
+
+
+
+
+// Add active class on another page linked
+// ==========================================
+// $(window).on('load',function () {
+//     var current = location.pathname;
+//     console.log(current);
+//     $('#navbarSupportedContent ul li a').each(function(){
+//         var $this = $(this);
+//         // if the current path is like this link, make it active
+//         if($this.attr('href').indexOf(current) !== -1){
+//             $this.parent().addClass('active');
+//             $this.parents('.menu-submenu').addClass('show-dropdown');
+//             $this.parents('.menu-submenu').parent().addClass('active');
+//         }else{
+//             $this.parent().removeClass('active');
+//         }
+//     })
+// });
+
+
+
+
+
+
+
+
+
+
+
+
+function prev() {
+	document.getElementById("slider-container").scrollLeft -= 270;
+}
+
+function next() {
+	document.getElementById("slider-container").scrollLeft += 270;
+}
+
+$(".slide img").on("click", function () {
+	$(this).toggleClass("zoomed");
+	$(".overlay").toggleClass("active");
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+function calculateBMI() {
+  let height = document.getElementById("height").value;
+  let weight = document.getElementById("weight").value;
+  let result = document.getElementById("bmiResult");
+
+  if (height === "" || weight === "") {
+    result.innerHTML = "Please enter height and weight";
+    result.style.color = "red";
+    return;
+  }
+
+  height = height / 100;
+  let bmi = (weight / (height * height)).toFixed(1);
+
+  if (bmi < 18.5) {
+    result.innerHTML = `BMI: ${bmi} (Underweight) – Eat calorie-rich & protein foods`;
+    result.style.color = "#ff9800";
+  } 
+  else if (bmi < 24.9) {
+    result.innerHTML = `BMI: ${bmi} (Normal) – Maintain balanced diet & exercise`;
+    result.style.color = "#4caf50";
+  } 
+  else {
+    result.innerHTML = `BMI: ${bmi} (Overweight) – Reduce sugar, exercise daily`;
+    result.style.color = "#f44336";
+  }
+}
